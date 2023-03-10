82fa8710d28584a462ac407130dcb46b14a2f83e
jdittert <jondittert@gmail.com>
2023-03-10 12:56:48 -0500

    Make function for printing intial gameboard

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
-const gameBoard = [];
+/* eslint no-plusplus: ["error", { "allowForLoopAfterthoughts": true }] */
+
+function GameBoard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        }
+    }
+
+    const getBoard = () => board;
+
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
+        console.log(boardWithCellValues);
+      };
+    
+      return { getBoard, printBoard };
+}
+
+function Cell() {
+    const value = 0;
+
+    const getValue = () => value;
+    return {
+        getValue
+    };
+}
 
 const playerFactory = (playerName, sign) => ({ playerName, sign });
+
+const game = GameBoard();
+game.printBoard();
 
