d9995e0656640ffce268e1ed1f5e16af4fd398a0
jdittert <jondittert@gmail.com>
2023-03-11 15:13:37 -0500

    Write rudimentary function for populating html game board

--- script.js
+++ script.js
@@ -16,20 +16,34 @@ function GameBoard() {
 
     const printBoard = () => {
         const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
+        
+        // Surely there's a way to do this without creating another array
+        const boardForPrinting = [];
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < columns; j++) {
+                boardForPrinting.push(boardWithCellValues[i][j]);
+            }
+        }
+        
+        for (let i = 1; i < 10; i++) {
+            document.getElementById(`game-box-${i}`).innerText = `${boardForPrinting[i - 1]}`;
+        }
         console.log(boardWithCellValues);
       };
     
-      return { getBoard, printBoard };
+      function Cell() {
+        const value = 0;
+    
+        const getValue = () => value;
+        return {
+            getValue
+        };
+    }
+
+    return { getBoard, printBoard };
 }
 
-function Cell() {
-    const value = 0;
 
-    const getValue = () => value;
-    return {
-        getValue
-    };
-}
 
 const playerFactory = (playerName, sign) => ({ playerName, sign });
 
